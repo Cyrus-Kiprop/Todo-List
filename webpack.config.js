2d48c78b0290d491be4c4b83a2ce06a2dee867ec
kip <cyruskiprop254@gmail.com>
2020-07-04 00:26:04 +0300

    configure webpack for development build

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,29 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  mode: 'development',
+  entry: { app: './src/index.js' },
+
   output: {
-    filename: 'main.js',
+    filename: 'app.bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+
+  devtool: 'inline-source-map',
+
+  devServer: {
+    contentBase: './dist',
+  },
+
+  plugins: [
+    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+    new HtmlWebpackPlugin({
+      title: 'Development',
+    }),
+  ],
+
   module: {
     rules: [
       {
