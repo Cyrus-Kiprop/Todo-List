5da785bbe975778fbb9e46aed27c6238deea8072
kip <cyruskiprop254@gmail.com>
2020-07-03 23:26:42 +0300

    :wrench: Webpack loaders
    add css, sass, scss, and file loaders

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,16 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.(sass|scss|css)$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: ['file-loader'],
+      },
+    ],
+  },
 };
