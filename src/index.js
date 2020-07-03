71cb1f078dc257e964ecc706ad0348334ec736f9
Shubham Raj <shubham14p3@gmail.com>
2020-07-03 23:18:14 +0530

    fixed linter

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import _ from 'lodash';
 
-  function component() {
-    const element = document.createElement('div');
+function component() {
+  const element = document.createElement('div');
 
 
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-    return element;
-  }
+  return element;
+}
 
-  document.body.appendChild(component());
+document.body.appendChild(component());
