2904a64da5617f69f0f8502ebb1a2c3060620a3b
Shubham Raj <shubham14p3@gmail.com>
2020-07-07 03:19:09 +0530

    add footer links

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,5 +1,35 @@
 import make from '../utils';
 
-const footer = make('div', 'navbar navbar-light nav_bar');
+const footer = make('div', 'footer-wrapper container');
 
+const footerItemWrapper = make('div','row');
+const footerItem1 = make('div', 'col-xs-12 col-sm-6 footer-brand');
+footerItem1.append(document.createTextNode('Todo-List'));
+
+const footerItem2 = make('div', 'col-xs-12 col-sm-6 social');
+const follow = make('h2');
+follow.appendChild(document.createTextNode('Follow:'));
+const facebook = make('a','footer-link my-3', undefined,{href:'#'});
+facebook.appendChild(document.createTextNode('Facebook'));
+const twitter = make('a', 'footer-link my-3', undefined, { href: '#' });
+twitter.appendChild(document.createTextNode('Twitter'));
+const github = make('a', 'footer-link my-3', undefined, { href: '#' });
+github.appendChild(document.createTextNode('Github'));
+const linkedin = make('a', 'footer-link my-3', undefined, { href: '#' });
+linkedin.appendChild(document.createTextNode('Linkedin'));
+footerItem2.appendChild(follow);
+footerItem2.appendChild(facebook);
+footerItem2.appendChild(twitter);
+footerItem2.appendChild(github);
+footerItem2.appendChild(linkedin);
+
+
+// const footerItem3 = make('div', 'col-xs-12 col-sm-6 editorial');
+// footerItem3.appendChild(document.createTextNode('Todo-List'));
+
+footerItemWrapper.appendChild(footerItem1);
+footerItemWrapper.appendChild(footerItem2);
+// footerItemWrapper.appendChild(footerItem3);
+
+footer.appendChild(footerItemWrapper)
 export default footer;
