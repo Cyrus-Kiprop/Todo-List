25e08e658987ff7dd752078c7221f8e68117fad8
Shubham Raj <shubham14p3@gmail.com>
2020-07-14 00:31:41 +0530

    removed in line comments

--- src/components/Container.js
+++ src/components/Container.js
@@ -6,22 +6,12 @@ import listWrapper from './todoListItems/todoListItems';
 import addListItem from './todoListItems/addTodoListItem';
 
 let container = utils.make('div', 'container-fluid');
-
-// nav
-// container = utils.appendBulkChild(container, [nav]);
-
-// body content
 let contentWrapper = utils.make('div', 'content-wrapper');
-// contentWrapper.appendChild(listWrapper);
-
-// left section of the main content
 const aside = utils.make('aside', 'left-aside');
 aside.appendChild(asideContainer);
 
-// right section of th main section
 let main = utils.make('main', 'body-content');
 
-// main.appendChild(nav);
 main = utils.appendBulkChild(main, [nav, listWrapper]);
 contentWrapper = utils.appendBulkChild(contentWrapper, [aside, main]);
 
