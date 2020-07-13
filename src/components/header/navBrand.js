30ea75aa5d9e458c568d73c1c10def2d970add26
Shubham Raj <shubham14p3@gmail.com>
2020-07-14 00:57:54 +0530

    add logo

--- src/components/header/navBrand.js
+++ src/components/header/navBrand.js
@@ -3,8 +3,10 @@ import utils from '../utils';
 const logo = utils.make('a', 'navbar navbar-light bg-light');
 
 const imgLogo = utils.make('img', 'navbar-brand', undefined, {
-  src: 'assets/images/todo-logo.png',
+  src:
+    'https://icons-for-free.com/iconfiles/png/512/checklist+documents+marks+todo+icon-1320184207350021670.png',
   alt: 'logo',
+  height: '50px',
 });
 
 logo.appendChild(imgLogo);
