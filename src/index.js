0781b235c7c9a24d6020685cabdf05c7981ec8f1
Shubham Raj <shubham14p3@gmail.com>
2020-07-14 00:31:48 +0530

    removed in line comments

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import './assets/styles/reset.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/styles/main.css';
 
-
-//  module importation
 import container from './components/Container';
 
 const todoList = document.getElementById('root');
